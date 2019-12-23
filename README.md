e16ef5c59fcb7acc236e891fcac07ec117017851
Elliot Alderson <fs0c131y@protonmail.com>
2019-12-23 11:37:18 +0100

    Initial contribution

---
+++ README.md
@@ -0,0 +1,20 @@
+# OpenBucket
+A small to dump content of an open bucket
+
+## Usage
+<pre>
+usage: download_bucket bucket_url
+
+positional arguments:
+  bucket_url               Url of the bucket to download
+</pre>
+ 
+## Installation
+  1. (Optional) `virtualenv venv && source ./venv/bin/activate`
+  2. `pip install -r requirements.txt`
+  
+## Contributing
+It's a quick and dirty project so issues are welcome and pull requests are appreciated.
+
+### Contributors
+  * [fs0c131y](https://twitter.com/fs0c131y)
---
+++ download_bucket.py
@@ -0,0 +1,64 @@
+import os
+import random
+import sys
+import xml.etree.ElementTree as ElementTree
+
+import requests
+
+from os import path
+
+
+def create_out_folder():
+    out_folder = ""
+    for i in range(1000):
+        out_folder = os.getcwd() + '/out/' + str(i)
+        if not os.path.exists(out_folder):
+            os.makedirs(out_folder)
+            break
+
+    return out_folder
+
+
+def download_file(bucket_url, url, out_folder):
+    file_name = url.rsplit('/', 1)[1]
+    print('[+] Download of ' + file_name)
+    r = requests.get(bucket_url + '/' + url)
+
+    if path.exists(out_folder + '/' + file_name):
+        file_name = str(random.randint(0, 100)) + '_' + file_name
+
+    with open(out_folder + '/' + file_name, 'wb') as f:
+        f.write(r.content)
+
+
+def parse_xml(bucket_list_result, bucket_url, out_folder):
+    tree = ElementTree.parse(bucket_list_result)
+    root = tree.getroot()
+
+    for elem in root:
+        for sub_elem in elem:
+            if sub_elem.tag == 'Key' and '.' in sub_elem.text:
+                download_file(bucket_url, sub_elem.text, out_folder)
+
+
+def retrieve_bucket_list_result(url, out_folder):
+    print('[+] Download bucket list result of ' + url)
+    r = requests.get(url)
+
+    with open(out_folder + '/content.xml', 'wb') as f:
+        f.write(r.content)
+
+
+def main():
+    if len(sys.argv) >= 2:
+        bucket_url = sys.argv[1]
+        out_folder = create_out_folder()
+        retrieve_bucket_list_result(bucket_url, out_folder)
+        parse_xml(out_folder + '/content.xml', bucket_url, out_folder)
+
+    else:
+        print('Usage: python download_bucket.py <bucket_url>')
+
+
+if __name__ == '__main__':
+    main()
